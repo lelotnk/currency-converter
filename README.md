4b66e126890a0f6dd84ab8bd57f82c8fbfd23283
Marco Tanaka <marco@ultrahaus.com>
2023-05-01 23:45:16 -0300

    npm registry tests and updates

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Currency Converter
 
 Conversor de moedas utilizando a API do Banco Central do Brasil
+
+
+```
+import { Converter } from './dist/index';
+
+Converter.convert("USD", "BRL", 1)
+    .then(response =>  console.log(response));
+```
+
+```
+const { Converter } = require('./dist');
+
+Converter.convert("USD", "BRL", 1)
+    .then(response =>  console.log(response));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export * from './UseCases/ConvertCurrencies';
+import ConvertCurrencies from "./UseCases/ConvertCurrencies";
+import BCBConverter from "./services/BCBConverter";
+
+export class Converter {
+    static async convert(currencyCodeFrom: string, currencyCodeTo: string, amount: number, date?: string) {
+        const converter = new BCBConverter();
+
+        const convertCurrenciesUseCase = new ConvertCurrencies(converter);
+
+        return await convertCurrenciesUseCase.execute(currencyCodeFrom, currencyCodeTo, amount, date);
+    }
+}
